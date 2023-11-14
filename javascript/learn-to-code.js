bac880e109722f4a2720ec502a8929627d3feb40
Bely <114695270+ImBely@users.noreply.github.com>
2023-11-14 09:32:59 -0400

    Add files via upload

---
+++ javascript/learn-to-code.js
@@ -0,0 +1,19 @@
+/*The logic is that this variable is saved in the memory of my PC (ram) in a random place,or also in chrome and then by PC*/
+
+/*var name = "Bely"; /*With comillas is var type string*/
+/*var age = 23; /*var number*/ 
+/*var shootingSocre = 22.6;
+
+var message = "Hi, my name is " + name + " and I am" + age + " years old";*/
+
+/* == checking if the value is the same. ==== checking if the value and the type is the same (number and string ["string entre comillas"])*/ 
+
+/* && both conditions have to be true.
+    || (alt+124) or, tiene que aplicar true alguna de las dos condiciones para que aparezca lo que quiero que aparezca*/
+console.log(message);
+
+/*if (nikeSBShoes <= myAccountBalance) {
+    myAccountBalance -= nikeSBShoes;  /*The -= is a shorthand operator for subtracting the value on the right (in this case, nikeSBShoes) */ 
+   /* console.log("We just bought");
+} Remember index = 0*/
+
